69f555d7a19ad1cca2267420b204870a0d1ca755
kalwalt <info@kalwaltart.it>
2019-02-18 19:14:19 +0100

    added logo in the nav bar

--- src/components/Navi/index.js
+++ src/components/Navi/index.js
@@ -1,53 +1,59 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import logo from '../../../content/images/Walter_Perdan_visual_artist_logo.svg'
 
 class Navi extends React.Component {
   render() {
     const { location, title } = this.props
     return (
-      <nav className="navbar navbar-expand navbar-dark flex-column flex-md-row bg-primary">
-        <div className="container">
-          <Link className="text-center" to="/">
-            <h1 className="navbar-brand mb-0">{title}</h1>
-          </Link>
-          <div className="navbar-nav-scroll">
-            <ul className="navbar-nav bd-navbar-nav flex-row">
-              <li
-                className={
-                  location.pathname === '/' ? 'nav-item active' : 'nav-item'
-                }
-              >
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
-              </li>
-              <li
-                className={
-                  location.pathname === '/profile/'
-                    ? 'nav-item active'
-                    : 'nav-item'
-                }
-              >
-                <Link to="/profile/" className="nav-link">
-                  Profile
-                </Link>
-              </li>
-              <li
-                className={
-                  location.pathname === '/blog/'
-                    ? 'nav-item active'
-                    : 'nav-item'
-                }
-              >
-                <Link to="/blog/" className="nav-link">
-                  Blog
-                </Link>
-              </li>
-            </ul>
+      <div>
+        <Link className="text-center" to="/">
+          <img src={logo} alt="Walter Perdan logo" className="Logo" />
+        </Link>
+        <nav className="navbar navbar-expand navbar-dark flex-column flex-md-row bg-primary">
+          <div className="container">
+            <Link className="text-center" to="/">
+              <h1 className="navbar-brand mb-0">{title}</h1>
+            </Link>
+            <div className="navbar-nav-scroll">
+              <ul className="navbar-nav bd-navbar-nav flex-row">
+                <li
+                  className={
+                    location.pathname === '/' ? 'nav-item active' : 'nav-item'
+                  }
+                >
+                  <Link to="/" className="nav-link">
+                    Home
+                  </Link>
+                </li>
+                <li
+                  className={
+                    location.pathname === '/profile/'
+                      ? 'nav-item active'
+                      : 'nav-item'
+                  }
+                >
+                  <Link to="/profile/" className="nav-link">
+                    Profile
+                  </Link>
+                </li>
+                <li
+                  className={
+                    location.pathname === '/blog/'
+                      ? 'nav-item active'
+                      : 'nav-item'
+                  }
+                >
+                  <Link to="/blog/" className="nav-link">
+                    Blog
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="navbar-nav flex-row ml-md-auto d-none d-md-flex" />
           </div>
-          <div className="navbar-nav flex-row ml-md-auto d-none d-md-flex" />
-        </div>
-      </nav>
+        </nav>
+      </div>
     )
   }
 }
